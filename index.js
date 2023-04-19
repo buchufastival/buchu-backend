1957312e75d19fa2b05732f8f8ef3cd4d9bd5cdc
Chell2358 <forestser00@gmail.com>
2023-04-19 14:02:13 +0900

    api request 수정

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ const express = require('express')
 const fs = require('fs')
 const app = express();
 const request = require('request')
+const path = require('path')
 const mysql = require('mysql')
 const apidata = require('./api.json')
 require('dotenv').config();
 var apiJson = "";
-var pageno = 1;
 
-const options = {
-  url : process.env.FASTIVAL_API_URL,
-  qs : {
-    serviceKey : process.env.URL_KEY_DECODING,
-    //보낼 때 자체적으로 encoding 을 하기 때문에 decoding 된 key로 전달을 해줘야 한다고 한다.
-    numOfRows : 10,
-    pageNo : pageno,
-    resultType : 'json',
-    //resultType은 json으로 줘서 보기 편하게 해준다.
-  }
-}
-
-app.get('/get/api', (req,res) => {
-  apiRequest();
-  //res.send(apidata)
+app.get('/', (req,res) => {
+    res.send('/')
+    console.log('/ page')
 })
 
-const apiRequest = () => {
+app.get('/getapi/:pageNo', (req,res) => {
+  apiRequest(req.params.pageNo);
+  console.log('testlog')
+  res.send(apidata)
+})
+
+const apiRequest = (pageNo) => {
+  console.log(pageNo)
+  let options = {
+    url : process.env.FASTIVAL_API_URL,
+    qs : {
+      serviceKey : process.env.URL_KEY_DECODING,
+      //보낼 때 자체적으로 encoding 을 하기 때문에 decoding 된 key로 전달을 해줘야 한다고 한다.
+      numOfRows : 10,
+      pageNo : pageNo,
+      resultType : 'json',
+      //resultType은 json으로 줘서 보기 편하게 해준다.
+    }
+  }
   request(options, (err, res, body) => {
     //console.log(body)
     apiJson = JSON.stringify(body)
@@ -34,10 +40,10 @@ const apiRequest = () => {
     //console.log(apiJson)
     fs.writeFileSync('api.json',apiJson)
   })
-  
-  
 }
 
-app.listen(8035, () => {
-  console.log('http://localhost:8035')
+
+
+app.listen(8008, () => {
+  console.log('http://localhost:8008')
 })
