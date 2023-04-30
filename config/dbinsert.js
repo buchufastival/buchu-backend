9e4944ef617825b59e3e64567f7cbe82aa6a188e
Chell2358 <forestser00@gmail.com>
2023-05-01 08:30:12 +0900

    insert문 수정

--- config/dbinsert.js
+++ config/dbinsert.js
@@ -1,3 +1,3 @@
 module.exports = {
-   sql : 'insert into fastival_kr(title,subtitle,main_place,addr1,addr2,cntct_tel,homepage_url,trfc_info,usage_day,usage_day_week_and_time,usage_amount,main_img_normal,main_img_thumb,itemcntnts) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+   sql : 'insert into fastival_kr(title,subtitle,main_place,addr1,addr2,cntct_tel,homepage_url,trfc_info,usage_day,usage_day_week_and_time,usage_amount,main_img_normal,main_img_thumb,itemcntnts) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?); ',
 }
