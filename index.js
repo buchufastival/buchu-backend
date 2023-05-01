aab14fc6c5c4ba582ae2365849c1b79a072c85a2
Chell2358 <forestser00@gmail.com>
2023-05-02 08:38:19 +0900

    insert 수정

--- index.js
+++ index.js
@@ -3,24 +3,77 @@ const fs = require('fs')
 const app = express();
 const request = require('request')
 const path = require('path')
-const mysql = require('mysql')
 const apidata = require('./api.json')
 require('dotenv').config();
+const mysql = require('mysql')
+const dbconfig = require('./config/dbconfig')
+const connection = mysql.createConnection(dbconfig);
+const dbinsert = require('./config/dbinsert');
 var apiJson = "";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connection.connect(
+  console.log('Succeed to connect MySQL')
+);
 
 app.get('/', (req,res) => {
     res.send('/')
     console.log('/ page')
 })
 
-app.get('/getapi/:pageNo', (req,res) => {
+app.get('/admin', (req,res) => {
+  res.send('admin page')
+})
+
+app.get('/fastivals/:pageNo', (req,res) => {
+  //console.log(req.params.pageNo)
   apiRequest(req.params.pageNo);
-  console.log('testlog')
   res.send(apidata)
 })
 
+app.get('/sqlfastivals', (req,res) => {
+  connection.query('select * from fastival_kr', (err, row, fields) => {
+    if(err) throw err;
+    res.send(row)
+  })
+})
+
+app.post('/newfastival', (req,res) => {
+  var insertsql = dbinsert.sql;
+  var values = [
+    req.body.title,
+    req.body.subtitle,
+    req.body.main_place,
+    req.body.addr1,
+    req.body.addr2,
+    req.body.cntct_tel,
+    req.body.homepage_url,
+    req.body.trfc_info,
+    req.body.usage_day,
+    req.body.usage_day_week_and_time,
+    req.body.usage_amount,
+    req.body.main_img_normal,
+    req.body.main_img_thumb,
+    req.body.itemcntnts
+    ]
+    console.log(values)
+    connection.query(insertsql,values, (err, row, fields) => {
+      if(err) throw err;
+      console.log('succeed insert');
+    })
+})
+
+app.delete('/deletefastival/:id', (req,res) => {
+  connection.query(`delete from fastival_kr where api_id = ${req.params.id}`, (err, row, fields) => {
+    if(err) throw err;
+    console.log('succeed delete');
+  })
+})
+
 const apiRequest = (pageNo) => {
-  console.log(pageNo)
+  //console.log(pageNo)
   let options = {
     url : process.env.FASTIVAL_API_URL,
     qs : {
@@ -33,16 +86,11 @@ const apiRequest = (pageNo) => {
     }
   }
   request(options, (err, res, body) => {
-    //console.log(body)
     apiJson = JSON.stringify(body)
     apiJson.replace('+', ' ')
     apiJson.replace('/\n/g', ' ')
-    //console.log(apiJson)
-    fs.writeFileSync('api.json',apiJson)
   })
 }
-
-
 
 app.listen(8008, () => {
   console.log('http://localhost:8008')
